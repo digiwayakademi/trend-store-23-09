2c2c834b8cffe23c9e47eb188608b67e4dc6768f
digiwayakademi <destek@digiway.com.tr>
2024-09-23 23:32:20 +0300

    trend-store-23-09
    DIGIWAY Akademi Hafta içi Yazılım Grubu

---
+++ trend-store/src/App.jsx
@@ -0,0 +1,16 @@
+
+import Hero from "./components/Hero/Hero"
+import Header from "./components/Layout/Header"
+
+function App() {
+  
+
+  return (
+    <div className='App'>      
+      <Header/>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
